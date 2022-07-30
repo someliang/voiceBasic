9fd62ad6a255e749a83356415029998af01d1ea8
someLiang <viso.liang@qq.com>
2022-07-30 12:12:58 +0800

    项目初始化

---
+++ sample/mscV5PlusDemo/src/main/java/com/iflytek/mscv5plusdemo/SpeechApp.java
@@ -0,0 +1,75 @@
+package com.iflytek.mscv5plusdemo;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
+public class SpeechApp extends Application {
+
+    @Override
+    public void onCreate() {
+        init();
+        // 应用程序入口处调用,避免手机内存过小,杀死后台进程后通过历史intent进入Activity造成SpeechUtility对象为null
+        // 注意：此接口在非主进程调用会返回null对象，如需在非主进程使用语音功能，请增加参数：SpeechConstant.FORCE_LOGIN+"=true"
+        // 参数间使用“,”分隔。
+        // 设置你申请的应用appid
+
+        // 注意： appid 必须和下载的SDK保持一致，否则会出现10407错误
+
+        StringBuffer param = new StringBuffer();
+        param.append("appid=" + getString(R.string.app_id));
+        param.append(",");
+        // 设置使用v5+
+        param.append(SpeechConstant.ENGINE_MODE + "=" + SpeechConstant.MODE_MSC);
+        SpeechUtility.createUtility(SpeechApp.this, param.toString());
+        super.onCreate();
+    }
+
+
+    private void init() {
+        // 打印日志
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                Log.d("Activity Resumed ----- ", activity.getClass().getName());
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+}
